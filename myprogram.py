e305c4df41cb96c4de78c6e548fb4bbfcd8604b4
Jatin Gupta <jack5432109@users.noreply.github.com>
2020-07-05 18:47:34 -0500

    fixes #3
    accept input from two sources- filename passed in command line arguments and STDIN.
    supports commands
     - python myprogram.py < input.txt
     - python myprogram.py input.txt

--- myprogram.py
+++ myprogram.py
@@ -1,18 +1,23 @@
 
 import sys
 
+def magix(line):
+    print (line)
 
-def magix(file_name):
+def magix_this_file_name(file_name):
     try:
         this_file = open(file_name, "r")
         lines = this_file.readlines()
-        return lines
+        for line in lines:
+            magix(line)
     except IOError as e:
         return e
-
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         file_names = sys.argv[1:]
         for file_name in file_names:
-            magix(file_name)
+            magix_this_file_name(file_name)
+    else:
+        for line in sys.stdin:
+            magix(line)
